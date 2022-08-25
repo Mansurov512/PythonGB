7eab675f1949062e404132c07395017fbab7ed08
MM <do9jng@gmail.com>
2022-08-25 18:39:22 +0300

    Ещё пара задач

---
+++ PythonTasks/PythonTasks/11-20 Tasks/13.py
@@ -0,0 +1,22 @@
+﻿# 13 Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+
+first = "еще"
+second = "ещещеще"
+counter = 0
+
+#print(len(first))
+#print(len(second))
+
+for i in range(len(second)):
+    if i < len(second) - len(first):
+        for j in range(len(first)):
+                if second[i + j] == first[j]:
+                    if j == len(first) - 1:
+                        counter += 1
+                    continue
+                else:
+                    break
+    else:
+        break
+
+print(counter)
