32f65ef29d21193a603123f9001498c9838f298c
MM <do9jng@gmail.com>
2022-09-04 19:34:05 +0300

    Сделал 8ую задачу.

---
+++ PythonTasks/PythonTasks/21-30/27.py
@@ -0,0 +1,13 @@
+﻿# Строка содержит набор чисел. Показать большее и меньшее число
+# Символ-разделитель - пробел
+
+
+def minAndMaxInStr(strOfNum: str) -> tuple:
+    listOfNum = strOfNum.split()
+    li = [int(el) for el in listOfNum]
+    return min(li), max(li)  # Сергей сказал что не стоит использовть функции min и max 
+                             # если не знаешь как они работают - могут спросить на собеседовании.
+
+
+print(minAndMaxInStr('234 34 78 349'))
+
---
+++ PythonTasks/PythonTasks/21-30/28.py
@@ -0,0 +1,30 @@
+﻿#Найти корни квадратного уравнения Ax² + Bx + C = 0
+#Математикой
+
+#def SolveQuadraticEquation (a, b, c):
+
+#    d = b**2-4*a*c #вычисляем дискриминант
+
+#    if d >= 0:
+#         x1 = (-b + d**0.5)/(2*a)
+#         x2 = (-b - d**0.5)/(2*a)
+#         print(x1, x2)
+#         return x1, x2
+#    else:
+#        print("Уравнение корней не имеет")
+#        return "Уравнение корней не имеет"
+
+
+
+#SolveQuadraticEquation(10, 2, 10)
+
+
+
+  # x = (-b ± √(b^2-4ac))/2a. При D > 0, уравнение имеет два корня; при D = 0, один корень; при D ˂ 0, уравнение корней не имеет.
+
+dev f (x):
+    with open("hello.txt", "r") as file:
+        str1 = file.readline()
+        str1.split = 
+    for number in file:
+        print(line, end="")
