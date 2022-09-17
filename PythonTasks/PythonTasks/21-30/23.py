d99a11029f2b4b728f3339530f67bcb3764ee1c8
MM <do9jng@gmail.com>
2022-09-17 22:55:39 +0300

    3е ДЗ 2ое задание(23)

---
+++ PythonTasks/PythonTasks/21-30/23.py
@@ -0,0 +1,16 @@
+﻿#23. Напишите программу, которая найдёт произведение пар чисел списка.
+#Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+#Пример:
+# [2, 3, 4, 5, 6] => [12, 15, 16];
+# [2, 3, 5, 6] => [12, 15]
+
+def ex23(listOfNums):
+    result = []
+    lengthOfList = len(listOfNums)
+    for i in range(lengthOfList // 2 + lengthOfList % 2):
+        result.append(listOfNums[i] * listOfNums[lengthOfList - 1 - i])
+    return result
+
+print(ex23([2, 3, 4, 5, 6]))
+print(ex23([2, 3, 5, 6]))
+print(ex23([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
