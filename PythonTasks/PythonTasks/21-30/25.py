2b7df83e3ff3721fba9207b250d58cca2b622693
MM <do9jng@gmail.com>
2022-09-18 17:15:57 +0300

    3е ДЗ 4ое задание(25)

---
+++ PythonTasks/PythonTasks/21-30/25.py
@@ -0,0 +1,16 @@
+﻿#25. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#Пример:
+#- 45 -> 101101
+#- 3 -> 11
+#- 2 -> 10
+
+def ex25(number):
+     result = ''
+     while number != 0:
+         result += str(number % 2)
+         number //= 2
+     return result
+
+print(ex25(45))
+print(ex25(3))
+print(ex25(2))
